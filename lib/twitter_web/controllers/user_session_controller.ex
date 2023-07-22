2d14250dc3798f7bf96e7a4c036ca32f7f3030b3
Samuel <kyohn9178@gmail.com>
2023-07-22 18:01:33 +0100

    allow login with username or email

--- lib/twitter_web/controllers/user_session_controller.ex
+++ lib/twitter_web/controllers/user_session_controller.ex
@@ -8,6 +8,10 @@ defmodule TwitterWeb.UserSessionController do
     create(conn, params, "Account created successfully!")
   end
 
+  def create(conn, %{"_action" => "login"} = params) do
+    create_registered(conn, params, "You are a piece of shit!")
+  end
+
   def create(conn, %{"_action" => "password_updated"} = params) do
     conn
     |> put_session(:user_return_to, ~p"/users/settings")
@@ -30,6 +34,22 @@ defmodule TwitterWeb.UserSessionController do
       conn
       |> put_flash(:error, "Invalid email or password")
       |> put_flash(:email, String.slice(email, 0, 160))
+      |> redirect(to: ~p"/users/log_in")
+    end
+  end
+
+  defp create_registered(conn, %{"user" => user_params}, info) do
+    %{"email_or_username" => email_or_username, "password" => password} = user_params
+
+    if user = Accounts.get_authenticated_user(email_or_username, password) do
+      conn
+      |> put_flash(:info, info)
+      |> UserAuth.log_in_user(user, user_params)
+    else
+      # In order to prevent user enumeration attacks, don't disclose whether the email is registered.
+      conn
+      |> put_flash(:error, "Invalid email or password")
+      |> put_flash(:email_or_username, String.slice(email_or_username, 0, 160))
       |> redirect(to: ~p"/users/log_in")
     end
   end
--- lib/twitter_web/live/user_login_live.ex
+++ lib/twitter_web/live/user_login_live.ex
@@ -15,8 +15,8 @@ defmodule TwitterWeb.UserLoginLive do
         </:subtitle>
       </.header>
 
-      <.simple_form for={@form} id="login_form" action={~p"/users/log_in"} phx-update="ignore">
-        <.input field={@form[:email]} type="email" label="Email" required />
+      <.simple_form for={@form} id="login_form" action={~p"/users/log_in?_action=login"} phx-update="ignore">
+        <.input field={@form[:email_or_username]} type="text" label="Email or username" required />
         <.input field={@form[:password]} type="password" label="Password" required />
 
         <:actions>
@@ -36,8 +36,8 @@ defmodule TwitterWeb.UserLoginLive do
   end
 
   def mount(_params, _session, socket) do
-    email = live_flash(socket.assigns.flash, :email)
-    form = to_form(%{"email" => email}, as: "user")
+    email_or_username = live_flash(socket.assigns.flash, :email_or_username)
+    form = to_form(%{"email_or_username" => email_or_username}, as: "user")
     {:ok, assign(socket, form: form), temporary_assigns: [form: form]}
   end
 end
