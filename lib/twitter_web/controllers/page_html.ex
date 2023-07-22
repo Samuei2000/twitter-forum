d3a015446e46a4bbe1972c0c29cc8f471cec71f7
Samuel <kyohn9178@gmail.com>
2023-07-23 00:27:02 +0100

    change home.html.heex

--- lib/twitter_web/controllers/page_html.ex
+++ lib/twitter_web/controllers/page_html.ex
@@ -2,4 +2,19 @@ defmodule TwitterWeb.PageHTML do
   use TwitterWeb, :html
 
   embed_templates "page_html/*"
+
+  attr :href, :string, required: true
+  attr :text, :string, required: true
+  attr :method, :string, default: "get"
+  def navbar_link(assigns) do
+    ~H"""
+    <.link
+      class="mb-2 sm:mb-0 sm:mr-5 text-gray-700 hover:text-gray-500"
+      href={@href}
+      method={@method}
+    >
+      <%= @text %>
+    </.link>
+    """
+  end
 end
