7c8bf965a77c96be6e61c9da582c7ead8938b942
Samuel <kyohn9178@gmail.com>
2023-07-23 06:39:39 +0100

    create category context

--- test/support/fixtures/forum_fixtures.ex
+++ test/support/fixtures/forum_fixtures.ex
@@ -35,4 +35,23 @@ defmodule Twitter.ForumFixtures do
 
     comment
   end
+
+  @doc """
+  Generate a unique category name.
+  """
+  def unique_category_name, do: "some name#{System.unique_integer([:positive])}"
+
+  @doc """
+  Generate a category.
+  """
+  def category_fixture(attrs \\ %{}) do
+    {:ok, category} =
+      attrs
+      |> Enum.into(%{
+        name: unique_category_name()
+      })
+      |> Twitter.Forum.create_category()
+
+    category
+  end
 end
--- test/twitter/forum_test.exs
+++ test/twitter/forum_test.exs
@@ -118,4 +118,58 @@ defmodule Twitter.ForumTest do
       assert %Ecto.Changeset{} = Forum.change_comment(comment)
     end
   end
+
+  describe "categories" do
+    alias Twitter.Forum.Category
+
+    import Twitter.ForumFixtures
+
+    @invalid_attrs %{name: nil}
+
+    test "list_categories/0 returns all categories" do
+      category = category_fixture()
+      assert Forum.list_categories() == [category]
+    end
+
+    test "get_category!/1 returns the category with given id" do
+      category = category_fixture()
+      assert Forum.get_category!(category.id) == category
+    end
+
+    test "create_category/1 with valid data creates a category" do
+      valid_attrs = %{name: "some name"}
+
+      assert {:ok, %Category{} = category} = Forum.create_category(valid_attrs)
+      assert category.name == "some name"
+    end
+
+    test "create_category/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Forum.create_category(@invalid_attrs)
+    end
+
+    test "update_category/2 with valid data updates the category" do
+      category = category_fixture()
+      update_attrs = %{name: "some updated name"}
+
+      assert {:ok, %Category{} = category} = Forum.update_category(category, update_attrs)
+      assert category.name == "some updated name"
+    end
+
+    test "update_category/2 with invalid data returns error changeset" do
+      category = category_fixture()
+      assert {:error, %Ecto.Changeset{}} = Forum.update_category(category, @invalid_attrs)
+      assert category == Forum.get_category!(category.id)
+    end
+
+    test "delete_category/1 deletes the category" do
+      category = category_fixture()
+      assert {:ok, %Category{}} = Forum.delete_category(category)
+      assert_raise Ecto.NoResultsError, fn -> Forum.get_category!(category.id) end
+    end
+
+    test "change_category/1 returns a category changeset" do
+      category = category_fixture()
+      assert %Ecto.Changeset{} = Forum.change_category(category)
+    end
+  end
 end
